5d965e1ba61702c124d9faba39710cacb1ee053b
dev.darshana991 <dev.darshana92@gmail.com>
2020-12-31 22:21:34 +0530

    minor

--- src/main/java/com/dev/crudStarterPack/dto/EmployeeMapper.java
+++ src/main/java/com/dev/crudStarterPack/dto/EmployeeMapper.java
@@ -1,11 +1,17 @@
 package com.dev.crudStarterPack.dto;
 import com.dev.crudStarterPack.model.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
     List<EmployeeDTO> employeeToDto(List<Employee> employee );
+
+    EmployeeDTO  employeeToEmployeeDto(Employee employee);
+
+    Employee employeeDtoToEmployee(EmployeeDTO employeeDTO);
 }
 
 
